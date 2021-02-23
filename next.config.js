7d7786f4467736ff7a67b4387f287349cd9ac2ed
Juan Villacorta <juanvillacortac@gmail.com>
2021-02-22 20:15:32 -0400

    Preact

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const withPlugins = require('next-compose-plugins')
 const dev = process.env.NODE_ENV !== 'production'
 
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack']
