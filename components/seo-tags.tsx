9589b6556fc1db13e9eeb4c99aee85aad0647cff
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-13 19:41:48 -0400

    SEO

--- components/seo-tags.tsx
+++ components/seo-tags.tsx
@@ -1,16 +1,12 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { getAbsoluteURL } from '@/lib/utils'
 
 export interface SeoTagsProps {
   title?: string
   brandTitle?: string
   description?: string
   type?: string
-}
-
-const getAbsoluteURL = (path: string) => {
-  const baseURL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
-  return baseURL + path
 }
 
 const SeoTags = ({
@@ -23,15 +19,17 @@ const SeoTags = ({
   return (
     <Head>
       <title>{title ? `${title} | ${brandTitle}` : brandTitle}</title>
-      <meta property="og:title" content={title}/>
+      <meta property="og:title" content={title || brandTitle}/>
+      <meta property="twitter:title" content={title || brandTitle}/>
 
       <meta name="description" content={description}/>
       <meta property="og:description" content={description}/>
 
       <meta property="og:url" content={getAbsoluteURL(pathname)}/>
       <meta property="og:type" content={type}/>
+      <meta property="og:site_name" content={brandTitle}/>
 
-      <meta name="twitter:card" content="summary"/>
+      <meta name="twitter:card" content="summary_large_image"/>
     </Head>
   )
 }
