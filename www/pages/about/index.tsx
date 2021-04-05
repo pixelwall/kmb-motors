6c3765df1d431bf1bf94ee2b58c3a4b97a7032d4
Juan Villacorta <juanvillacortac@gmail.com>
2021-04-05 00:33:07 -0400

    About us fix

--- www/pages/about/index.tsx
+++ www/pages/about/index.tsx
@@ -1,10 +1,10 @@
 import Page from '@/components/page'
 import Hero from './hero'
-import Contact from './contact'
+import Contact, { ContactProps } from './contact'
 import Map from './map'
 import Description, { AboutDescription } from './description'
 
-interface IndexProps extends AboutDescription {
+interface IndexProps extends AboutDescription, ContactProps {
   globalData?: any
 }
 
